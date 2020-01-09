65ab7e2d4cc97992f78908d8fb5f6f3af9b3bb47
Shwetsee Singh <Shwetsee Singh@SHWEITSEEE>
2020-01-09 16:03:30 +0530

    yjuyjyj

--- src/Demogit.java
+++ src/Demogit.java
@@ -1,6 +1,7 @@
 
 public class Demogit {
-public static void main(String[] args) {
-	System.out.println("hii","fufuy","gugi");
+public static void main(String[] args) 
+{
+	System.out.println("gugli , helo");
 }
 }
